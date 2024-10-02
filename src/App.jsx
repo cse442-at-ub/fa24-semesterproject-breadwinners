c609e0d7282828b1f27bbd683df08eba3e543435
Abir <abir@abirs-mbp.mynetworksettings.com>
2024-10-01 20:05:47 -0400

    homepage all components complete frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import RegisterPage from './components/Register/RegisterPage';
+import HomePage from './components/Home/HomePage';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <RegisterPage/>
+        <HomePage/>
       </div>
     </>
   )
