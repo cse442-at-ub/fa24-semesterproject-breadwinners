d1dcc378da311e2e73a7101bbb30bc0da8f631b2
Abir <abir@Abirs-MacBook-Pro.local>
2024-10-11 12:45:56 -0400

    setup backend and route for cattle

--- login_backend.php
+++ login_backend.php
@@ -6,7 +6,7 @@
 $servername = "localhost:3306"; 
 $username = "hassan4"; // Database username
 $password = "50396311"; // Database password
-$db_name = "hassan4_db"; // Database name
+$db_name = "cse442_2024_fall_team_y_db"; // Database name
 
 // Create connection to MySQL database
 $conn = new mysqli($servername, $username, $password, $db_name);
